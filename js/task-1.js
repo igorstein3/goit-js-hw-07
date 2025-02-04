f86c3895d0dd857ee387ac75a56d88ea93d9899b
igorstein3 <igorstein3@gmail.com>
2025-02-04 21:58:24 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,10 @@
 'use strict';
+const ulCategories = document.querySelector("#categories");
+const categories = ulCategories.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+categories.forEach((item) => {
+    const categoryTitle = item.querySelector("h2");
+    const categoryElements = item.querySelectorAll("ul li");
+    console.log(`Category: ${categoryTitle.textContent}`);
+    console.log(`Elements: ${categoryElements.length}`)
+})
