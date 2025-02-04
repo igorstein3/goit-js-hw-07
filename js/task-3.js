b5576335e443e254888a992da3eeb0d80ab313c0
igorstein3 <igorstein3@gmail.com>
2025-02-04 23:38:44 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,8 @@
 'use strict';
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+    const inputValue = event.currentTarget.value.trim();
+    if (inputValue.length > 0) { output.textContent = inputValue } else {output.textContent = "Anonymous"}
+});
