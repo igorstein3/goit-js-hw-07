4084a7185f6379bf6ac63f3d614352cdcc70e70c
igorstein3 <igorstein3@gmail.com>
2025-02-06 23:41:15 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
 'use strict';
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+    const data = {};
+    data.email = form.elements.email.value.trim();
+    data.password = form.elements.password.value.trim();
+  
+  if ( data.email === "" || data.password === "") {
+    return window.alert("All form fields must be filled in");
+  }
+  console.log(data);
+  form.reset();
+}
