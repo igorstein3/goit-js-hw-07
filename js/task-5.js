68cbdbd8978cb57dd4db95d63f20bec8578cbdc4
igorstein3 <igorstein3@gmail.com>
2025-02-07 00:54:30 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -4,3 +4,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const back = document.querySelector("body");
+const newColor = document.querySelector(".color");
+
+back.addEventListener("click", buttonClick);
+
+function buttonClick(event) {
+  back.style.backgroundColor = getRandomHexColor();
+  newColor.textContent = back.style.backgroundColor;
+}
+
